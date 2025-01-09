35951c724f88727615dad06f1d12f36ba8aeae59
thoratprashant <prashant.thorat@arkenea.com>
2025-01-09 12:48:06 +0530

    Update forgot password module

--- src/app/auth/forgot-password/forgot-password.component.ts
+++ src/app/auth/forgot-password/forgot-password.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password', 
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrl: './forgot-password.component.scss'
 })
 export class ForgotPasswordComponent {
+  private _formBuilder = inject(FormBuilder);
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
 
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  } 
+  onOtpChange(otp: string) { console.log('OTP changed:', otp); }
 }
