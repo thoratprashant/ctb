efe6df382234b16035b0fe7b685b3abf2d052481
thoratprashant <prashant.thorat@arkenea.com>
2025-01-14 10:11:35 +0530

    WIP > Add Hospital

--- src/app/super-admin/manage-hospitals/add-hospital/add-hospital.component.ts
+++ src/app/super-admin/manage-hospitals/add-hospital/add-hospital.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { merge } from 'rxjs';
 
 @Component({
   selector: 'app-add-hospital', 
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './add-hospital.component.scss'
 })
 export class AddHospitalComponent {
-
+  readonly hname = new FormControl('', [Validators.required]);
+  readonly adOne = new FormControl('', [Validators.required]);
+  readonly adTwo = new FormControl('', [Validators.required]);
+  readonly city = new FormControl('', [Validators.required]);
+  readonly state = new FormControl('', [Validators.required]);
+  readonly zipcode = new FormControl('', [Validators.required]);
+  readonly contactNumber = new FormControl('', [Validators.required]);
+  readonly email = new FormControl('', [Validators.required]); 
+  readonly fname = new FormControl('', [Validators.required]);
+  readonly lname = new FormControl('', [Validators.required]);
+  readonly haemail = new FormControl('', [Validators.required]); 
+  readonly mnumber = new FormControl('', [Validators.required]); 
+  
 }
